03fbc2d383056a9bff35cdf89eaef69659de52b3
Yarlagadda <SYARLAG1@depaul.edu>
2018-03-12 22:40:12 -0500

    changed theta calc to account of atan(x/o)

--- jacobi_eig_pca.py
+++ jacobi_eig_pca.py
@@ -1,4 +1,4 @@
-from math import sin, cos, atan
+from math import sin, cos, atan, pi
 import numpy as np
 
 # https://en.wikipedia.org/wiki/Jacobi_eigenvalue_algorithm
@@ -40,8 +40,14 @@ def jacobi_eig(M, ap=1e-6, rp=1e-5):
         # stopping condition
         if max_val < ap or max_val < rp*max_val:
             break
-        # calculate theta
-        theta = atan(M_updated[i,j]/(M_updated[j,j]-M_updated[i,i]))
+        # calculate angle of rotation (theta)
+        try:
+            theta = math.atan(2*M_updated[i,j]/(M_updated[j,j]-M_updated[i,i]))/2
+        except: # incase the denominator is 0
+            if M_updated[i,j] >= 0:
+                theta = math.pi/4
+            else:
+                theta = -math.pi/4
         # create corresponding Givens' Matrix
         Q = create_givens_mat(n,i,j,theta)
         # update Q (this will be the eigen vectors)
@@ -76,3 +82,71 @@ def pca(X, k, center=True, scale=True):
     eigen_vals, pcs = jacobi_eig(covM)
     # only return the top k pcs
     return np.dot(X, pcs[:k].T)
+
+
+########################## TESTING ###########################################
+# test jacobi_eig()
+import numpy as np
+# 1
+A = [[2, -1, 0], [-1, 2, -1], [0, -1, 2]] # symmetric psd
+# using numpy
+vals, vecs = np.linalg.eig(A)
+print(vals)
+#[ 3.41421356  2.          0.58578644]
+print(vecs.T)
+#[[ -5.00000000e-01   7.07106781e-01  -5.00000000e-01]
+# [ -7.07106781e-01   4.05405432e-16   7.07106781e-01]
+# [  5.00000000e-01   7.07106781e-01   5.00000000e-01]]
+# using above func
+vals, vecs = jacobi_eig(Matrix(A))
+print(vals)
+#[3.414213562373095, 1.9999999999999996, 0.5857864376269049]
+print(vecs)
+#[[0.5000000000000526, 0.7071067811864732, 0.5000000000000526], 
+# [0.7071067811865474, -4.629821043762799e-17, -0.7071067811865476], 
+# [0.4999999999999474, -0.7071067811866218, 0.4999999999999475]]
+
+# 2
+B = [[2, 1, 0], [1, 4, 1], [0, 1, 4]] # symmetric psd
+# using numpy
+vals, vecs = np.linalg.eig(B)
+print(vals)
+#[ 1.5188057   3.31110782  5.17008649]
+print(vecs.T)
+#[[ 0.88765034 -0.42713229  0.17214786]
+# [ 0.39711255  0.52065737 -0.75578934]
+# [ 0.23319198  0.73923874  0.63178128]]
+# using above func
+vals, vecs = jacobi_eig(Matrix(B))
+print(vals)
+#[5.170086486626034, 3.311107817465982, 1.5188056959079843]
+print(vecs)
+#[[0.8876503388301478, -0.42713228703499645, 0.17214785896716067], 
+# [0.39711254978700716, 0.5206573684395941, -0.7557893406837775], 
+# [0.2331919783705817, 0.7392387395569922, 0.6317812811106418]]
+
+
+# test pca()
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+iris_data = np.loadtxt('iris_data.csv',delimiter=',',usecols=range(4))
+# using sklearn
+scaled_iris = StandardScaler().fit_transform(iris_data) # center and scale
+res = PCA(n_components=2).fit_transform(scaled_iris)
+print(res)
+#[[-2.26454173  0.5057039 ]
+# [-2.0864255  -0.65540473]
+# [-2.36795045 -0.31847731]
+# ..., 
+# [ 1.52084506  0.26679457]
+# [ 1.37639119  1.01636193]
+# [ 0.95929858 -0.02228394]]
+# using above function
+X = Matrix(iris_data.tolist())
+res = pca(X, k=2, center=True, scale=True)
+print(res.tolist()[:3]) # displaying only first 3
+#[[-2.2645417284068214, 0.5057039035404838], 
+# [-2.086425500669378, -0.6554047279403764], 
+# [-2.3679504490768926, -0.31847731117093403]]
+
+###############################################################################
